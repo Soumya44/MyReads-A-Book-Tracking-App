66fc396abf8ed416b7cce313d490f09100d4d82e
Soumya <soumyaranjanb02@gmail.com>
2018-10-04 21:00:46 +0530

    Fixed Search page issue and Used filter/map

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import SearchPage from './components/SearchPage';
-import BooksList from './components/BooksList';
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+// import * as BooksAPI from './BooksAPI'
+import './App.css'
+import Home from './screens/Home/Home'
+import Search from './screens/Search/Search'
+
 
 class BooksApp extends React.Component {
   render() {
     return (
-      <div className="app">
-        <Route path="/" exact component={BooksList} />
-        <Route path="/search" component={SearchPage}/>
-      </div>
-    );
+      <Router>
+        <div className="app">
+          <Route exact path='/' component={Home} />
+          <Route exact path='/search' component={Search} />
+        </div>
+      </Router>
+    )
   }
 }
 
